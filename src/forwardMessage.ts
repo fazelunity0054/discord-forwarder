023183ef688b0187a7b3dc1cf5849aa77dc60326
Alg <33424336+algj@users.noreply.github.com>
2021-08-05 15:52:00 +0300

    Update forwardMessage.ts

--- src/forwardMessage.ts
+++ src/forwardMessage.ts
@@ -1,4 +1,4 @@
-import * as Discord from 'discord.js-self';
+import * as Discord from 'discord.js';
 import { ConfigOptions, SendableChannel } from './types';
 import { fetchWebhook } from "./webhookManager";
 
